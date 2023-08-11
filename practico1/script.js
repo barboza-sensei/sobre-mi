aed5003e81c363ea9ffc3c9693d0d6fb6bc1ced6
barboza-sensei <134016396+barboza-sensei@users.noreply.github.com>
2023-08-10 21:07:32 -0300

    modificaciones de funcionalidad

--- practico1/script.js
+++ practico1/script.js
@@ -1,48 +1,46 @@
-const operand1 = document.getElementById('operand1');
-const operand2 = document.getElementById('operand2');
-const operator = document.getElementById('operator');
-const calculateButton = document.getElementById('calculate');
-const resultElement = document.getElementById('result');
-const errorElement = document.getElementById('error');
+document.addEventListener("DOMContentLoaded", function() {
+  const operand1 = document.getElementById("operand1");
+  const operand2 = document.getElementById("operand2");
+  const operator = document.getElementById("operator");
+  const calculateBtn = document.getElementById("calculate");
+  const resultDiv = document.getElementById("result");
 
-const operator = document.getElementById('operator');
-const calculateButton = document.getElementById('calculate');
-
-calculateButton.addEventListener('click', () => {
-    const num1 = parseFloat(prompt('Ingresa el primer número:'));
-    const num2 = parseFloat(prompt('Ingresa el segundo número:'));
-
-    if (isNaN(num1) || isNaN(num2)) {
-        alert('Ingresa números válidos en ambos campos.');
-        return;
-    }
-
+  calculateBtn.addEventListener("click", function() {
+    const num1 = parseFloat(operand1.value);
+    const num2 = parseFloat(operand2.value);
     const selectedOperator = operator.value;
+    
+    if (isNaN(num1) || isNaN(num2)) {
+      alert("Ingresa operandos válidos.");
+      return;
+    }
+    
     let result;
-
+    
     switch (selectedOperator) {
-        case 'add':
-            result = num1 + num2;
-            break;
-        case 'subtract':
-            result = num1 - num2;
-            break;
-        case 'multiply':
-            result = num1 * num2;
-            break;
-        case 'divide':
-            if (num2 === 0) {
-                alert('No se puede dividir por cero.');
-                return;
-            }
-            result = num1 / num2;
-            break;
+      case "add":
+        result = num1 + num2;
+        break;
+      case "subtract":
+        result = num1 - num2;
+        break;
+      case "multiply":
+        result = num1 * num2;
+        break;
+      case "divide":
+        if (num2 === 0) {
+          alert("No se puede dividir por cero.");
+          return;
+        }
+        result = num1 / num2;
+        break;
     }
-
+    
     if (!isFinite(result)) {
-        alert('El resultado es demasiado grande o pequeño.');
-        return;
+      alert("El resultado es demasiado grande o pequeño.");
+      return;
     }
-
-    console.log(`El resultado es: ${result}`);
+    
+    resultDiv.textContent = `Resultado: ${result}`;
+  });
 });
